de90f0a433f9b8348cbe7950d735a6deb164f707
Sean Luce <lucesean@gmail.com>
2016-12-05 01:24:02 -0500

    Added docker php image on port 80 moved httpd to 8080 and added index.php template

--- LAMPDeployCookbook/recipes/docker_install.rb
+++ LAMPDeployCookbook/recipes/docker_install.rb
@@ -5,11 +5,15 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 directory '/tmp/htdocs' do
-  action :create
+    action :create
 end
 
 template '/tmp/htdocs/index.html' do
   source 'index.html.erb'
+end
+
+template '/tmp/htdocs/index.php' do
+  source 'index.php.erb'
 end
 
 docker_service 'default' do
@@ -31,7 +35,21 @@ end
 docker_container 'my_httpd' do
   repo 'httpd'
   tag 'latest'
-  port '80:80'
-  volumes [ '/tmp/htdocs:/usr/local/apache2/htdocs/' ]
+  port '8080:80'
+  volumes [ '/tmp/htdocs:/usr/local/apache2/htdocs' ]
 end
 
+# Pull latest image
+docker_image 'php' do
+  tag '7.0-apache'
+  action :pull_if_missing
+  notifies :redeploy, 'docker_container[my_php]'
+end
+
+# Run container exposing ports
+docker_container 'my_php' do
+  repo 'php'
+  tag '7.0-apache'
+  port '80:80'
+  volumes [ '/tmp/htdocs:/var/www/html' ]
+end
